db5916eb1ec1641156266c6bcd8bbfbd740cb6ec
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-04 21:10:11 -0300

    Aula 01 - Use Strict

---
+++ 01-es6+/es6.js
@@ -0,0 +1,25 @@
+'use strict'
+/*function showMessage() {
+    personName = 'Danilo'
+    console.log(`Hello, ${personName}`)
+}
+
+showMessage()*/
+
+/*class Student {
+    get point(){
+        return 7
+    }
+}
+
+let student = new Student()
+student.point = 'teste'*/
+
+//delete window.document
+
+function sum(a, a, c){
+    return a + c
+}
+
+let result = sum(10,20,30)
+console.log(result)
