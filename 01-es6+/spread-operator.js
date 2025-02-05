f85696b7f5933f4e09ef8f4352a783a86264cf0c
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-04 21:42:42 -0300

    Aula 05 - Spread Operator

---
+++ 01-es6+/spread-operator.js
@@ -0,0 +1,16 @@
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+console.log(...numbers)
+
+const users = [
+    {
+        name: 'John',
+        email: 'john@example.com',
+    },
+    {
+        name: 'Danilo',
+        email: 'danilo@example.com',
+    }
+]
+
+console.log(...users)
