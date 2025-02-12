9159ccf97e0583f445e67276f40703162e8ed6dc
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-11 23:19:02 -0300

    Aula 48 - Webpack dev server

--- 09-bundler/src/js/index.js
+++ 09-bundler/src/js/index.js
@@ -1,4 +1,4 @@
 import "../css/styles.css"
 import {title} from './components.js'
 
-title("Hello World")
+title("Hello!!")
--- webpack.config.js
+++ webpack.config.js
@@ -8,23 +8,30 @@ module.exports = {
         path: path.resolve(`${__dirname}/09-bundler`, 'dist')
     },
     mode: 'development',
+    devServer: {
+        static: {
+            directory: path.join(`${__dirname}/09-bundler`, 'dist', 'index.html')
+        },
+        port: 3000,
+        open: true,
+    },
     plugins: [new HTMLWebpackPlugin()],
     module: {
         rules: [
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
-                exclude: /node_modules/,
+                exclude: /node_modules/
             },
             {
                 test: /\.js$/i,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: [['@babel/preset-env', { targets: 'defaults' }]],
+                        presets: [['@babel/preset-env', {targets: 'defaults'}]]
                     }
                 },
-                exclude: /node_modules/,
+                exclude: /node_modules/
             }
         ]
     }
