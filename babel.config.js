27572c860b2fa1e0e7cfb5c24910f5876166d3af
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-11 21:32:21 -0300

    Aula 42 - Configurações adicionais para o preset do Babel

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 const presets = [
-    "@babel/preset-env",
+    [
+        "@babel/preset-env",
+        {
+            targets: {
+                "edge" : "17",
+                "firefox" : "60",
+                "chrome" : "67",
+                "safari" : "11.1",
+            },
+            "useBuiltins": "usage",
+        }
+    ]
 ]
 
 module.exports = {presets}
