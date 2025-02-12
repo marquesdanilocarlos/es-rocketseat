b36dc84c8851c312c710eb272abba45f375c6d1f
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-11 21:16:42 -0300

    Aula 38 - Configuração do Babel

---
+++ 08-compilacao/main.js
@@ -0,0 +1,9 @@
+//Comando do babel
+//./node_modules/.bin/babel 08-compilacao/main.js --out-dir 08-compilacao/dist
+class User {
+    constructor (email) {
+        this.email = email
+    }
+}
+
+let user = new User('marquesdanilocarlos@gmail.com')
