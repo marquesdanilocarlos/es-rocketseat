4d4090b7f29176d0a569e149a9af7233af0926f3
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-12 22:08:42 -0300

    Aula 57 - Adição do Babel no Webpack

--- 10-projeto/webpack.config.js
+++ 10-projeto/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
             patterns: [
                 {
                     from: path.resolve(__dirname, './src/assets'),
-                    to: path.resolve(__dirname, './dist/assets'),
+                    to: path.resolve(__dirname, './dist/src/assets'),
                 }
             ]
         })
@@ -37,6 +37,16 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
         ]
     }
