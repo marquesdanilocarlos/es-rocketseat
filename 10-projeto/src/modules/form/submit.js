91e28f8fad7dbe379f5165630141d463980518a7
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-13 15:53:03 -0300

    Aula 64 - Criação do objeto de agendamento para envio para a API

--- 10-projeto/src/modules/form/submit.js
+++ 10-projeto/src/modules/form/submit.js
@@ -2,6 +2,7 @@ import dayjs from 'dayjs'
 
 const form = document.querySelector("form")
 const selectedDate = document.querySelector("#date")
+const clientName = document.querySelector("#client")
 
 const today = dayjs(new Date()).format('YYYY-MM-DD')
 selectedDate.value = today
@@ -10,4 +11,30 @@ selectedDate.min = today
 form.onsubmit = (e) => {
     e.preventDefault();
 
+    try {
+        const name = clientName.value.trim()
+
+        if (!name) {
+            return alert('Informe o nome do cliente')
+        }
+
+        const hourSelected = document.querySelector(".hour-selected")
+
+        if (!hourSelected) {
+            return alert('Informe o horário')
+        }
+
+        const [hour] = hourSelected.innerText.split(":")
+        const when = dayjs(selectedDate.value).add(hour, 'hour')
+        const id = new Date().getTime()
+
+        const schedule = {
+            id,
+            name,
+            when
+        }
+    } catch (error) {
+        alert('Não foi possível realizar o agendamento.')
+        console.log(error)
+    }
 }
