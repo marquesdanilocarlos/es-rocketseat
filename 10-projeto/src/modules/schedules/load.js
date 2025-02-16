770fe10ab31d04fe65dd02eff8b027332b523057
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-15 23:41:53 -0300

    Aula 69 - Início do método de consulta dos agendamentos do dia

--- 10-projeto/src/modules/schedules/load.js
+++ 10-projeto/src/modules/schedules/load.js
@@ -1,7 +1,12 @@
 import {hoursLoad} from '../form/hours-load.js'
+import {fetchSchedulesByDay} from '../../services/fetch-schedules.js'
 
 const selectedDate = document.querySelector('#date')
-export function schedulesDay() {
+export async function schedulesDay() {
     const date = selectedDate.value
+
+    const dailySchedule = await fetchSchedulesByDay({date})
+    console.log(dailySchedule)
+
     hoursLoad(date);
 }
